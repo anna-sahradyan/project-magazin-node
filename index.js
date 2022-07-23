b40a6b769d76ca212a14b727909bb0bbf9441c50
anna <a.sahradyan72@gmail.com>
2022-07-23 20:08:58 +0400

    create pug

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ const app = express();
 /**
  * public= name directory where save static
  */
+app.set("views engine","pug");
 app.use(express.static("public"))
 app.listen(4000, () => {
     console.log("Backend is starting !!!")
 });
-app.get("/", (req, res) => {
-    res.end("hello")
-});
+// app.get("/", (req, res) => {
+//     res.end("hello")
+// });
 app.get("/", (req, res) => {
     console.log("load/")
-    res.render("index.html")
+    res.render("main.pug",{
+        foo:4,
+        bar:7
+    })
 });
