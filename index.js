42b8b02fbbe230c53d58705b09f08c258ec9e5ae
anna <a.sahradyan72@gmail.com>
2022-08-02 23:59:32 +0400

    create mustard-ui

--- index.js
+++ index.js
@@ -8,11 +8,7 @@ app.set("views engine", "pug");
 app.use(express.static("public"));
 
 const conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "market",
-    password: "mysql1972",
-    port: 3306
+    host: "localhost", user: "root", database: "market", password: "mysql1972", port: 3306
 
 })
 conn.connect(err => {
@@ -24,9 +20,6 @@ conn.connect(err => {
     }
 });
 
-app.listen(4000, () => {
-    console.log("Backend is starting !!!")
-});
 
 app.get("/", (req, res) => {
     let query = "SELECT * FROM goods"
@@ -40,11 +33,41 @@ app.get("/", (req, res) => {
         }
         console.log(JSON.parse(JSON.stringify(goods)))
         res.render("main.pug", {
-            foo: 4,
-            bar: 7,
-            goods:JSON.parse(JSON.stringify(goods))
+            foo: 4, bar: 7, goods: JSON.parse(JSON.stringify(goods))
         });
 
     })
 
+});
+app.get('/cat', (req, res) => {
+     let catId = req.query.id;
+      console.log(req.query.id);
+    res.render('cat.pug', {});
+    let cat = new Promise((resolve, reject) => {
+        conn.query('SELECT * FROM category WHERE id = ' + catId, (err, result) => {
+            if (err) {
+                reject (err);
+            }
+            resolve(result);
+        })
+    })
+    let goods = new Promise((resolve, reject) => {
+        conn.query('SELECT * FROM goods WHERE category = ' + catId, (err, result) => {
+            if (err) {
+                reject (err);
+            }
+            resolve(result);
+        })
+    })
+    Promise.all([cat,goods]).then(function (value) {
+
+console.log(value[1]);
+        // res.render("cat", {
+        //     foo: 'hello CATEGORY',
+        // });
+    })
+});
+
+app.listen(4000, () => {
+    console.log("Backend is starting !!!")
 });
