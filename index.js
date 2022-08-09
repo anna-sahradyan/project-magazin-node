e6fcc37575a66c47c0f6c0e8c199613b1f273cdc
anna <a.sahradyan72@gmail.com>
2022-08-09 16:33:40 +0400

    create card.pug file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 /**
  * public= name directory where save static
  */
-app.set("views engine", "pug");
+app.set("view engine", "pug");
 app.use(express.static("public"));
 
 const conn = mysql.createConnection({
@@ -31,42 +31,53 @@ app.get("/", (req, res) => {
         for (let i = 0; i < result.length; i++) {
             goods[result[i]['id']] = result[i];
         }
-        console.log(JSON.parse(JSON.stringify(goods)))
+        // console.log(JSON.parse(JSON.stringify(goods)))
         res.render("main.pug", {
-            foo: 4, bar: 7, goods: JSON.parse(JSON.stringify(goods))
+            foo: 4,
+            bar: 7,
+            goods: JSON.parse(JSON.stringify(goods))
         });
 
-    })
+    });
 
 });
+
+//!block  category
+
+
 app.get('/cat', (req, res) => {
-     let catId = req.query.id;
-      console.log(req.query.id);
-    res.render('cat.pug', {});
+    let catId = req.query.id;
     let cat = new Promise((resolve, reject) => {
         conn.query('SELECT * FROM category WHERE id = ' + catId, (err, result) => {
             if (err) {
-                reject (err);
+                reject(err);
             }
             resolve(result);
-        })
-    })
+        });
+    });
     let goods = new Promise((resolve, reject) => {
         conn.query('SELECT * FROM goods WHERE category = ' + catId, (err, result) => {
             if (err) {
-                reject (err);
+                reject(err);
             }
             resolve(result);
-        })
-    })
-    Promise.all([cat,goods]).then(function (value) {
+        });
+    });
+    Promise.all([
+        cat.catch((error) => error),
+        goods.catch((error) => error),
+    ]).then(function ( values) {
+        console.log(values[0])
 
-console.log(value[1]);
-        // res.render("cat", {
-        //     foo: 'hello CATEGORY',
-        // });
+        res.render("cat.pug", {
+            cat:JSON.parse(JSON.stringify(values[0])),
+            goods:JSON.parse(JSON.stringify(values[1]))
+        });
+
     })
 });
+
+
 
 app.listen(4000, () => {
     console.log("Backend is starting !!!")
